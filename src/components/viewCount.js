8507ad68effe4b2990d4df55a83f23064e1b697c
Nipitpon Chantada <nppi3enz@gmail.com>
2019-04-23 23:20:18 +0700

    change to 3m

--- src/components/viewCount.js
+++ src/components/viewCount.js
@@ -38,7 +38,7 @@ class viewCount extends React.Component {
     onCollectionUpdate = (querySnapshot) => {
         const tables = [];
         querySnapshot.forEach((doc) => {
-            var goal = 2000000
+            var goal = 3000000
             var calper = Math.floor(100*doc.data().data.viewCount/goal)
             if(calper > 10){
                 this.setState({
@@ -86,7 +86,7 @@ class viewCount extends React.Component {
                     class="progress-footer">
                         <div
                         className="bubble">
-                        2M
+                        3M
                         </div>
                     </div>
                 </div>
